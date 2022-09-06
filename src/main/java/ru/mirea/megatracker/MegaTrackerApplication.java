fc05bbf10d1d26a4f165c0383d38d480fffd384f
ArtemKovaa <artem.kovalenko190203@gmail.com>
2022-09-06 20:34:33 +0300

    Project created
    - Added required dependencies

---
+++ src/main/java/ru/mirea/megatracker/MegaTrackerApplication.java
@@ -0,0 +1,13 @@
+package ru.mirea.megatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mirea/megatracker/MegaTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.megatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
