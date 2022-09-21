9f1b11388ce553893698fb0e88ae7ba79c54e7da
syasyayas <ya.sizkov@gmail.com>
2022-09-21 22:33:21 +0300

    feat: add fields for class PriceInfo

--- src/main/java/ru/mirea/megatracker/API/PriceInfo.java
+++ src/main/java/ru/mirea/megatracker/API/PriceInfo.java
@@ -1,6 +1,36 @@
 package ru.mirea.megatracker.API;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public abstract class PriceInfo {
+
+    @JsonProperty(value = "PRICE")
+    private double price;
+
+    @JsonProperty(value = "CHANGE24HOURS")
+    private double changeDay;
+
+    @JsonProperty(value = "CHANGEPCT24HOUR")
+    private double changeDayPct;
+
+    @JsonProperty(value = "MKTCAP")
+    private long marketCap;
+
+    @JsonProperty(value = "HIGHDAY")
+    private double highDay;
+
+    @JsonProperty(value = "LOWDAY")
+    private double lowDay;
+
 
 }
