66c736705f0643af65cf798175772a8b0ec67ca7
ArtemKovaa <artem.kovalenko190203@gmail.com>
2022-09-24 09:40:19 +0300

    fix: remove redundant code and change method name in WebClientConfig

--- src/main/java/ru/mirea/megatracker/config/WebClientConfig.java
+++ src/main/java/ru/mirea/megatracker/config/WebClientConfig.java
@@ -7,10 +7,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
-    private String ApiKey = "cb0b9f6468d47f38977dc89ec588774b38b9715028f2c91ef2969dfd70391cab";
-
     @Bean
-    public WebClient getWebClientBuilder(){
+    public WebClient webClient(){
         return WebClient.builder().
                 baseUrl("https://min-api.cryptocompare.com/").
                 build();
