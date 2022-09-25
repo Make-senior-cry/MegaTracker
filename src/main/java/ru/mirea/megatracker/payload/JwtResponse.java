84ccd6399ab98a9a36e16655952c75d1c07b07af
ArtemKovaa <artem.kovalenko190203@gmail.com>
2022-09-25 12:40:35 +0300

    feat: add entity for jwt response

---
+++ src/main/java/ru/mirea/megatracker/payload/JwtResponse.java
@@ -0,0 +1,14 @@
+package ru.mirea.megatracker.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class JwtResponse {
+    private String accessToken;
+    private String refreshToken;
+    private String email;
+}
