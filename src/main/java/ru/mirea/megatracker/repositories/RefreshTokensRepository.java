adb1e179a8ab4c8585c7388008236c4c4028f6ba
ArtemKovaa <artem.kovalenko190203@gmail.com>
2022-09-25 13:25:07 +0300

    feat: add service for refresh token

--- src/main/java/ru/mirea/megatracker/repositories/RefreshTokensRepository.java
+++ src/main/java/ru/mirea/megatracker/repositories/RefreshTokensRepository.java
@@ -1,6 +1,7 @@
 package ru.mirea.megatracker.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 import ru.mirea.megatracker.models.RefreshToken;
 import ru.mirea.megatracker.models.User;
@@ -11,5 +12,6 @@ import java.util.Optional;
 public interface RefreshTokensRepository extends JpaRepository<RefreshToken, Integer> {
     Optional<RefreshToken> findByToken(String token);
 
+    @Modifying
     int deleteByUser(User user);
 }
