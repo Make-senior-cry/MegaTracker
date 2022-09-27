c438d497cdb42ba321ceaaf5f1f86cc2de3864d3
syasyayas <ya.sizkov@gmail.com>
2022-09-27 22:23:33 +0300

    feat: add class CoinHistoryPrice to store info about price of coin at a time

---
+++ src/main/java/ru/mirea/megatracker/api/CoinHistoryPrice.java
@@ -0,0 +1,33 @@
+package ru.mirea.megatracker.api;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CoinHistoryPrice {
+
+    @JsonProperty(value = "time")
+    private String time;
+
+    @Setter
+    @JsonProperty(value = " close")
+    private long closePrice;
+
+    public void setTime(long time) {
+        this.time = convertUnixDateToString(time);
+    }
+
+    private String convertUnixDateToString(long unixDate){
+        Date realDate = new Date(unixDate);
+        return String.valueOf(realDate);
+    }
+}
