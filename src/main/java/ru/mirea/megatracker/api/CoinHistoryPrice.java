31c56ac6a0aa33b38f63b5d1020b24646c74d561
syasyayas <ya.sizkov@gmail.com>
2022-09-27 22:37:19 +0300

    feat: add convertation to CoinPriceHistoryDTO in CoinHistoryPrice

--- src/main/java/ru/mirea/megatracker/api/CoinHistoryPrice.java
+++ src/main/java/ru/mirea/megatracker/api/CoinHistoryPrice.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import ru.mirea.megatracker.dto.CoinPriceHistoryDTO;
 
 import java.util.Date;
 
@@ -29,5 +30,11 @@ public class CoinHistoryPrice {
     private String convertUnixDateToString(long unixDate){
         Date realDate = new Date(unixDate);
         return String.valueOf(realDate);
+    }
+
+    public void convertToDto(CoinPriceHistoryDTO dto, priceDif){
+        dto.setClosingPrice(this.closePrice);
+        dto.setTime(this.time);
+        dto.setPriceDif(priceDif);
     }
 }
