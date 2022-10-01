5b5545104ea528b00d96d3121ab90d65ecd66485
ArtemKovaa <artem.kovalenko190203@gmail.com>
2022-10-01 19:42:42 +0300

    feat: add findByUser method in NotesRepository

--- src/main/java/ru/mirea/megatracker/repositories/NotesRepository.java
+++ src/main/java/ru/mirea/megatracker/repositories/NotesRepository.java
@@ -3,10 +3,12 @@ package ru.mirea.megatracker.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.mirea.megatracker.models.Note;
+import ru.mirea.megatracker.models.User;
 
 import java.util.Optional;
 
 @Repository
 public interface NotesRepository extends JpaRepository<Note, String> {
     Optional<Note> findByTicker(String ticker);
+    Optional<Note> findByUser(User user);
 }
