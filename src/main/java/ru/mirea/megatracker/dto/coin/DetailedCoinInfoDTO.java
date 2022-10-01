c8679fba5c4f7c6a89fdf777b95d3c461d7ec6b7
ArtemKovaa <artem.kovalenko190203@gmail.com>
2022-10-01 22:06:14 +0300

    fix: name of key to pass field isFavorite in DetailedCoinInfoDTO

--- src/main/java/ru/mirea/megatracker/dto/coin/DetailedCoinInfoDTO.java
+++ src/main/java/ru/mirea/megatracker/dto/coin/DetailedCoinInfoDTO.java
@@ -1,5 +1,6 @@
 package ru.mirea.megatracker.dto.coin;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,8 +9,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class DetailedCoinInfoDTO extends BaseCoinDTO{
-
+    @JsonProperty(value = "isFavorite")
     private boolean isFavorite;
+
     private String note;
 
     private float highDayPrice;
