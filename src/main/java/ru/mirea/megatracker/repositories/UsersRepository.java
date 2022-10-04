702d31b66c15767778ba01e565160e268179e27c
ArtemKovaa <artem.kovalenko190203@gmail.com>
2022-10-04 13:44:43 +0300

    feat: add method of finding users email by password in UsersRepository

--- src/main/java/ru/mirea/megatracker/repositories/UsersRepository.java
+++ src/main/java/ru/mirea/megatracker/repositories/UsersRepository.java
@@ -11,4 +11,6 @@ public interface UsersRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
     Boolean existsByEmail(String email);
     Optional<Integer> findIdByEmail(String email);
+
+    String findPasswordByEmail(String email);
 }
