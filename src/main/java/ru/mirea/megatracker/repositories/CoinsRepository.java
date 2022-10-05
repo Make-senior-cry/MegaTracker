427a53f87101a23812f6e6090220923c68480a5b
syasyayas <ya.sizkov@gmail.com>
2022-10-05 17:40:36 +0300

    fix: change query of findAllRisingWithFilters method

--- src/main/java/ru/mirea/megatracker/repositories/CoinsRepository.java
+++ src/main/java/ru/mirea/megatracker/repositories/CoinsRepository.java
@@ -9,13 +9,14 @@ import java.util.List;
 
 public interface CoinsRepository extends JpaRepository<Coin, Integer> {
     Coin findByTicker(String ticker);
+
     boolean existsByTicker(String ticker);
 
     @Query("select c from Coin c where c.currentPrice >= :minPrice and c.currentPrice <= :maxPrice order by c.id")
     List<Coin> findAllWithFilters(@Param("minPrice") float minPrice,
                                   @Param("maxPrice") float maxPrice);
 
-    @Query("select c from Coin c where c.currentPrice >= :minPrice and c.currentPrice <= :maxPrice and c.deltaPrice > 0" +
+    @Query("select c from Coin c where c.currentPrice >= :minPrice and c.currentPrice <= :maxPrice and c.deltaPrice > 0.001" +
             "order by c.id")
     List<Coin> findAllRisingWithFilters(@Param("minPrice") float minPrice,
                                         @Param("maxPrice") float maxPrice);
