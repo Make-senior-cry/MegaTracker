3dd7e4c7d991f42301bb150a6c23661c6d9092fb
Vladislav Knyshov <inova136@gmail.com>
2022-10-14 12:01:09 +0300

    feat: move API_URL to env variables

--- src/main/java/ru/mirea/megatracker/config/WebClientConfig.java
+++ src/main/java/ru/mirea/megatracker/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package ru.mirea.megatracker.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,10 +8,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${api.url}")
+    private String apiUrl;
+
     @Bean
     public WebClient webClient() {
         return WebClient.builder().
-                baseUrl("https://min-api.cryptocompare.com/data/").
+                baseUrl(apiUrl).
                 build();
     }
 
