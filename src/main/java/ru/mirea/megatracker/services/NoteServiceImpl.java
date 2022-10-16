a4f0eb017715b524a550c83271a0eb86555d9e75
Vladislav Knyshov <inova136@gmail.com>
2022-10-16 19:43:14 +0300

    chore: remove unused import

--- src/main/java/ru/mirea/megatracker/services/NoteServiceImpl.java
+++ src/main/java/ru/mirea/megatracker/services/NoteServiceImpl.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import ru.mirea.megatracker.exceptions.NoteNotFoundException;
 import ru.mirea.megatracker.exceptions.UserNotFoundException;
 import ru.mirea.megatracker.models.Note;
 import ru.mirea.megatracker.models.User;
