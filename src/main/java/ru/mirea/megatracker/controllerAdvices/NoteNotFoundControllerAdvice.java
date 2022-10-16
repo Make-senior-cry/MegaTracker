87af7df64092b85e35530495ad5d7738234bab32
Vladislav Knyshov <inova136@gmail.com>
2022-10-16 19:42:29 +0300

    feat: add NoteNotFound exception & controllerAdvice

---
+++ src/main/java/ru/mirea/megatracker/controllerAdvices/NoteNotFoundControllerAdvice.java
@@ -0,0 +1,21 @@
+package ru.mirea.megatracker.controllerAdvices;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import ru.mirea.megatracker.exceptions.NoteNotFoundException;
+
+@ControllerAdvice
+public class NoteNotFoundControllerAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(NoteNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String linkNotFoundHandler(NoteNotFoundException exception) {
+        return exception.getMessage();
+    }
+}
